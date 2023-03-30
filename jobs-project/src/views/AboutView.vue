becc3877b9d1062785145cbbb981ec704de3da1a
Mateusz Czosnyka <mateuszczosn@gmail.com>
2023-03-30 21:24:13 +0200

    Vue Router p2

---
+++ jobs-project/src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maiores dignissimos obcaecati id enim eius mollitia iste ea esse nobis vitae voluptates, soluta ipsum, dolore aliquam accusantium praesentium cupiditate iusto.</p>
+  </div>
+</template>
---
+++ jobs-project/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, quia. Reiciendis itaque animi voluptates commodi ex hic sunt perferendis. Possimus odit laborum id sequi quaerat repudiandae, nostrum fuga suscipit nobis?</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView'
+}
+</script>
